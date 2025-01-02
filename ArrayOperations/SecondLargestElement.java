132041c926f04739c49caa038b6034781f5c9cc4
Akshay Rasal <102125918+Akshay6449@users.noreply.github.com>
2025-01-02 16:58:37 +0530

    Update SecondLargestElement.java

--- ArrayOperations/SecondLargestElement.java
+++ ArrayOperations/SecondLargestElement.java
@@ -20,7 +20,6 @@ public class SecondLargestElement {
         for (int i = 0; i < n - 1; i++) {
             for (int j = i + 1; j < n; j++) {
                 if (arr[i] > arr[j]) {
-                    // Swap elements
                     int temp = arr[i];
                     arr[i] = arr[j];
                     arr[j] = temp;
